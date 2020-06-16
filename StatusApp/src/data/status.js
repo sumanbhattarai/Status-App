992bc1486239cb1aebcb493850ac8a0891d330e0
sumanbhattarai <sumanbhattaraiofficial@gmail.com>
2020-06-16 20:33:46 +0545

    removed all

--- StatusApp/src/data/status.js
+++
@@ -1,7 +0,0 @@
-import Status from "../model/status";
-
-export const  StatusCollection  = [
-    new Status( 's1' , [ 'c1' , 'c3' , 'c7'] , 'I am \'me\' and I won\'t change for anyone. '),
-    new Status( 's2' , [ 'c1' , 'c3' , 'c7'] , 'I am \'me\' and I won\'t change for anyone. '),
-    new Status( 's3' , [ 'c1' , 'c3' , 'c7'] , 'I am \'me\' and I won\'t change for anyone. ')
-]
--- StatusApp/src/model/status.js
+++
@@ -1,9 +0,0 @@
-class Status {
-    constructor( id , categoryId , status){
-        this.id = id ,
-        this.categoryId = categoryId ,
-        this.status = status
-    }
-}
-
-export default Status
