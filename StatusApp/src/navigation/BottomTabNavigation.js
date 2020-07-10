9d7bad3617fb68fb6a3dcb396a58e2a041c206db
sumanbhattarai <sumanbhattaraiofficial@gmail.com>
2020-07-10 15:07:41 +0545

    glitched fixed

--- StatusApp/src/navigation/BottomTabNavigation.js
+++ StatusApp/src/navigation/BottomTabNavigation.js
@@ -1,5 +1,6 @@
 import 'react-native-gesture-handler';
 import React from 'react'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
 import { NavigationContainer } from '@react-navigation/native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
@@ -12,32 +13,34 @@ import FavouriteStackNavigation from './FavouriteStackNavigation';
 const BottomTab = createBottomTabNavigator()
 export default function(){
     return(
-        <NavigationContainer>
-            <BottomTab.Navigator
-                tabBarOptions={{
-                    activeTintColor : Color.primary,
-                    labelStyle : {
-                        fontFamily :  Font.primary,
-                        paddingBottom : 5
-                    }
-                }}
-                
-            >
-                <BottomTab.Screen 
-                    name="Category" 
-                    component={StackNavigation}
-                    options={{
-                        tabBarIcon:({color , size})=> <FontAwesomeIcon icon={faBoxes} color={color} size={size} />
-                    }} 
-                />
-                <BottomTab.Screen 
-                    name="Favourite" 
-                    component={FavouriteStackNavigation} 
-                    options={{
-                        tabBarIcon : ({color ,size})=> <FontAwesomeIcon icon={ faStar } color={color} size={size} />
+        <SafeAreaProvider>
+            <NavigationContainer>
+                <BottomTab.Navigator
+                    tabBarOptions={{
+                        activeTintColor : Color.primary,
+                        labelStyle : {
+                            fontFamily :  Font.primary,
+                            paddingBottom : 5
+                        }
                     }}
-                />
-            </BottomTab.Navigator>
-        </NavigationContainer>
+                    
+                >
+                    <BottomTab.Screen 
+                        name="Category" 
+                        component={StackNavigation}
+                        options={{
+                            tabBarIcon:({color , size})=> <FontAwesomeIcon icon={faBoxes} color={color} size={size} />
+                        }} 
+                    />
+                    <BottomTab.Screen 
+                        name="Favourite" 
+                        component={FavouriteStackNavigation} 
+                        options={{
+                            tabBarIcon : ({color ,size})=> <FontAwesomeIcon icon={ faStar } color={color} size={size} />
+                        }}
+                    />
+                </BottomTab.Navigator>
+            </NavigationContainer>
+        </SafeAreaProvider>
     )
 }
